e33cfac8b511ad13a43556c12590add374691294
MohitGupta121 <guptamm.22@gmail.com>
2022-01-16 10:21:56 +0530

    Update Dialog Remote Config

---
+++ app/src/main/java/com/example/updatedialog/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.updatedialog;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textCiewCurrentVersion;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textCiewCurrentVersion = findViewById(R.id.tv);
+        textCiewCurrentVersion.setText("Current Version Code: " +getVersionCode());
+
+        HashMap<String, Object> defaultsRate = new HashMap<>();
+        defaultsRate.put("new_version_code", String.valueOf(getVersionCode()));
+
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(10) // change to 3600 on published app
+                .build();
+
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+        mFirebaseRemoteConfig.setDefaultsAsync(defaultsRate);
+
+        mFirebaseRemoteConfig.fetchAndActivate().addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+            @Override
+            public void onComplete(@NonNull Task<Boolean> task) {
+                if (task.isSuccessful()) {
+                    final String new_version_code = mFirebaseRemoteConfig.getString("new_version_code");
+
+                    if(Integer.parseInt(new_version_code) > getVersionCode())
+                        showTheDialog("com.facebook.lite", new_version_code );
+                }
+                else Log.e("MYLOG", "mFirebaseRemoteConfig.fetchAndActivate() NOT Successful");
+
+            }
+        });
+    }
+
+    private void showTheDialog(final String appPackageName, String versionFromRemoteConfig){
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Update")
+                .setMessage("This version is absolete, please update to version: "+versionFromRemoteConfig)
+                .setPositiveButton("UPDATE", null)
+                .show();
+
+        dialog.setCancelable(false);
+
+        Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=" + appPackageName)));
+                }
+                catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+            }
+        });
+    }
+
+    private PackageInfo pInfo;
+    public int getVersionCode() {
+        pInfo = null;
+        try {
+            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.i("MYLOG", "NameNotFoundException: "+e.getMessage());
+        }
+        return pInfo.versionCode;
+    }
+
+}
